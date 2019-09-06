37bc528e96ba19da77dbd2e3a30394717579a949
takumiw <w.tak.1229@gmail.com>
2019-09-06 16:09:31 +0900

    Update chap2/best_cow_line.py

--- chap2/best_cow_line.py
+++ chap2/best_cow_line.py
@@ -2,7 +2,26 @@ N = int(input())
 S = list(input())
 
 def solve():
-    pass
+    # S[a], S[a+1], ..., S[b]が残っている文字列
+    a = 0
+    b = N-1
+    while a <= b:
+        # 左から見た場合と右から見た場合を比較
+        left = False
+        for i in range(b-a+1):
+            if S[a+i] < S[b-i]:
+                left = True
+                break
+            elif S[a+i] > S[b-i]:
+                left = False
+                break
+        if left:
+            print(S[a], end='')
+            a += 1
+        else:
+            print(S[b], end='')
+            b -= 1
+    print()
 
 if __name__ == '__main__':
     solve()
